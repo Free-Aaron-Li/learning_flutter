8fb266beca6735ccbd9ddcc65bc66717ea5db0f9
aaron <communicate_aaron@outlook.com>
2025-02-17 23:30:48 +0800

    feat(初始化): 添加依赖项，优化结构 - 添加“轮播图”依赖 - 优化代码结构，对代码进行分开管理

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:learning_flutter/app.dart';
 
-import 'package:learning_flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
